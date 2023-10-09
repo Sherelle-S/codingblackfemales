4ece089762a0e6ed9ff0a52f03aba1638a407cf0
Sherelle <sherellie1@hotmail.com>
2023-10-09 12:19:44 +0100

    based on fizz buzz but caused a stack overflow

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -68,48 +68,80 @@ public class MyAlgoLogic implements AlgoLogic {
 
         // checks if market is open, this is left over from the timed logic that i was working on. if total orders are less than 15 we buy, if order count is divisible by 3 we sell and if active orders are greater than 6 we cancel.
         if(timedLogic.isMarketOpen()){
-            if(totalOrderCount < 15 ){
 
-                logger.info("[MYALGO] Aftr buy orders" + activeOrders.size());
-                logger.info("[MYALGO] After Buy total orders" + totalOrderCount);
+            for(totalOrderCount = 0; totalOrderCount < 16; totalOrderCount++){
+                if (totalOrderCount % 3 == 0 && totalOrderCount % 5 == 0){
+                     final var option = activeOrders.stream().findFirst();
+                    // option is an object
+                    logger.info("[MYALGO] Have:" + activeOrders.size() + " children, want 7, done.");
+                        if (option.isPresent()){
+                            logger.info("[MYALGO] Active orders are "+ activeOrders.size());
+                            // active orders is what we will track to cancel the orders
+                            var childOrder = option.get();
+                            // get the child order
+                            logger.info("[MYALGO] Cancelling order:" + childOrder);
+                            // logs info for child order
+                            logger.info("[MYALGO] Option is present, number of active orders are "+ activeOrders.size());
+                            return new CancelChildOrder(childOrder);
+                        }else if(totalOrderCount % 3 == 0){
+                            logger.info("[MYALGO] Aftr buy orders" + activeOrders.size());
+                            logger.info("[MYALGO] After Buy total orders" + totalOrderCount);
                             return new CreateChildOrder(Side.BUY, quantityBuy, priceBuy);
-            }
-            
-            if(totalOrderCount % 3 == 0){
- 
-                logger.info("[MYALGO] Adding order for" + quantitySELL + "@" + priceSELL);
-                logger.info("[MYALGO] Initial quantity" + quantitySELL + " price " + priceSELL);
-                logger.info("[MYALGO] Active orders" + activeOrders.size());
-                logger.info("[MYALGO] Total orders" + totalOrderCount);
+                        } else if (totalOrderCount % 5 == 0){
+                            logger.info("[MYALGO] Adding order for" + quantitySELL + "@" + priceSELL);
+                            logger.info("[MYALGO] Initial quantity" + quantitySELL + " price " + priceSELL);
+                            logger.info("[MYALGO] Active orders" + activeOrders.size());
+                            logger.info("[MYALGO] Total orders" + totalOrderCount);
 
-                return new CreateChildOrder(Side.SELL, quantitySELL, priceSELL);
+                            return new CreateChildOrder(Side.SELL, quantitySELL, priceSELL);
+                        }else{
+                            return NoAction.NoAction;
+                        }
             }
+           
+           
+        //     if(totalOrderCount < 15 ){
 
-             if(activeOrders.size() > 6){
-            final var option = activeOrders.stream().findFirst();
-            // option is an object
-             logger.info("[MYALGO] Have:" + activeOrders.size() + " children, want 7, done.");
-            if (option.isPresent()){
-                logger.info("[MYALGO] Active orders are "+ activeOrders.size());
-                // active orders is what we will track to cancel the orders
-                var childOrder = option.get();
-                // get the child order
-                logger.info("[MYALGO] Cancelling order:" + childOrder);
-                // logs info for child order
-                logger.info("[MYALGO] Option is present, number of active orders are "+ activeOrders.size());
-                return new CancelChildOrder(childOrder);
-            }else{
-                // this else is causing failure
-                return NoAction.NoAction;
-            }
- 
-        }else{
-            return NoAction.NoAction;
-        }
+        //         logger.info("[MYALGO] Aftr buy orders" + activeOrders.size());
+        //         logger.info("[MYALGO] After Buy total orders" + totalOrderCount);
+        //                     return new CreateChildOrder(Side.BUY, quantityBuy, priceBuy);
+        //     }
             
-        } else{
-            return NoAction.NoAction;
-        }
+        //     if(totalOrderCount % 3 == 0){
+ 
+        //         logger.info("[MYALGO] Adding order for" + quantitySELL + "@" + priceSELL);
+        //         logger.info("[MYALGO] Initial quantity" + quantitySELL + " price " + priceSELL);
+        //         logger.info("[MYALGO] Active orders" + activeOrders.size());
+        //         logger.info("[MYALGO] Total orders" + totalOrderCount);
+
+        //         return new CreateChildOrder(Side.SELL, quantitySELL, priceSELL);
+        //     }
+
+        //      if(activeOrders.size() > 6){
+        //     final var option = activeOrders.stream().findFirst();
+        //     // option is an object
+        //      logger.info("[MYALGO] Have:" + activeOrders.size() + " children, want 7, done.");
+        //     if (option.isPresent()){
+        //         logger.info("[MYALGO] Active orders are "+ activeOrders.size());
+        //         // active orders is what we will track to cancel the orders
+        //         var childOrder = option.get();
+        //         // get the child order
+        //         logger.info("[MYALGO] Cancelling order:" + childOrder);
+        //         // logs info for child order
+        //         logger.info("[MYALGO] Option is present, number of active orders are "+ activeOrders.size());
+        //         return new CancelChildOrder(childOrder);
+        //     }else{
+        //         // this else is causing failure
+        //         return NoAction.NoAction;
+        //     }
+ 
+        // }else{
+        //     return NoAction.NoAction;
+        // }
+            
+        // } else{
+        //     return NoAction.NoAction;
+        // }
     }
 
 
@@ -158,3 +190,7 @@ public class MyAlgoLogic implements AlgoLogic {
     //     } 
     // }
     }
+    return NoAction.NoAction;
+}
+
+}
